23ee092f7abba56948b929c27d0e555d5daa3eae
Tiago Redaelli <tiago.redaelli@gmail.com>
2019-11-15 15:21:44 +0100

    removed unusremed dependencies

--- commands/replay.js
+++ commands/replay.js
@@ -28,16 +28,17 @@ module.exports = class ReplayCommand {
 
 
         (async () => {  
-            
             var [result, replay] = await Promise.all([
                 wc3stats.fetchResultById(id, season), 
                 wc3stats.fetchReplayById(id, season) 
             ]);
+            if (replay === "No results found." || result === "No results found.") {
+                msg.channel.send(MessageUtils.error("Failed to find results for {" + args[0] + "}. This might be because it was posted during a previous season."));
+                return;
+            }
             result = result[0];
             var playerStr = "";
             var ratingStr = "";
-
-
             for (var i = 0; i < result.teams.length; i++) {
                 var team = result.teams[i];
                 for (var j = 0; j < team.players.length; j++) {
@@ -46,14 +47,11 @@ module.exports = class ReplayCommand {
                     ratingStr += ((player.change > 0)? "+": "") + player.change +"\n";
                 }
             }
-
             var title = result.key.map + " #" + result.replayId;
             var description = result.key.season;
             var timestamp = result.timestamp*1000;
-          
             var timeInSeconds = replay.data.game.recordLength;
             var gameDuration = new Date(null, null, null, null, null, timeInSeconds).toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
-
             const embdedResult = new Discord.RichEmbed()
                 .setColor(config.embedcolor)
                 .setTitle(title)
